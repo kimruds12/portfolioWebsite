6f786fb0c1ccd1bf030cb3024cd73e8c1a79e857
kimruds12 <kimrudsguston@gmail.com>
2026-06-02 01:33:52 +0800

    Latest and now ready to pass

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="px-4 pb-10 pt-6 sm:px-6 lg:px-8">
       <div className="mx-auto max-w-7xl border-t border-cyan-300/10 pt-6 text-center text-sm text-white/45">
-        <p>©2026 Design and Develop by Kim Ruds Guston</p>
+        <p>©2026 Designed and Developed by Kim Ruds Guston</p>
       </div>
     </footer>
   );
